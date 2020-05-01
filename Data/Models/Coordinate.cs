c254b6b0a9b152b022f29ad87067cc56f45b054a
filipgo <filipgow@gmail.com>
2020-05-01 22:37:21 +0200

    developing Coordinates.cs

--- Data/Models/Coordinate.cs
+++ Data/Models/Coordinate.cs
@@ -1,27 +1,46 @@
+using System;
+
 namespace Data.Models
 {
     public class Coordinate
     {
         public Coordinate(double value)
         {
-            value = Value;
-            
+            Value = value;
+
         }
         
-        public double Value { get; }
+        // ReSharper disable once MemberCanBePrivate.Global
+        public double Value { get;}
         
-        public int Degree { get; set; }
+        private int Degree { get; set; }
         
-        public int Minutes { get; set; }
+        private int Minutes { get; set; }
 
-        public int Seconds { get; set; }
+        private int Seconds { get; set; }
         
-        public int DecimalSeconds { get; set; }
+        private double DecimalSeconds { get; set; }
 
 
-        public string GetDmsValue() //DMS if Degree Minute Second format of Longitude/Latitude 
+        public string GetDmsValue(bool withDecimalSeconds = false) //DMS if Degree Minute Second format of Longitude/Latitude 
         {
             return $"";
+        }
+
+        private void CalculateDms()
+        {
+            //TODO compare with Floor()
+            var rest = Value%1;
+            Degree = Convert.ToInt32(Value - rest);
+
+            var rest2 = rest % (1.0 / 60);
+            Minutes = Convert.ToInt32(rest - rest2);
+
+            var rest3 = rest2 % (1.0 / 360);
+            Seconds = Convert.ToInt32(rest2 - rest3);
+
+            DecimalSeconds = Math.Round(rest3, 10);
+
         }
     }
 }
